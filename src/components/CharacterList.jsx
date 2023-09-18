047d78cbcc77cc5e101bb61c0092a1e0a4b0ccd6
Santiago Guzzo <santijguzzo@gmail.com>
2023-09-18 16:09:56 -0300

    colores

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -5,16 +5,23 @@ function NavPage(props) {
   return (
     <header className="d-flex justify-content-between align-items-center">
       <button
-        className="btn btn-primary btn-sm"
+        className="btn btn-primary btn-sm "
+        style={{ backgroundColor: "#08bd02" }}
         onClick={() => props.setPage(props.page - 1)}
       >
         Pagina {props.page - 1}
       </button>
       <form>
-        <input type="text" onChange={props.handleSearchUpdate} />
+        <input
+          className="search-input"
+          type="text"
+          onChange={props.handleSearchUpdate}
+          placeholder="Nombre del Personaje..."
+        />
       </form>
       <button
         className="btn btn-primary btn-sm"
+        style={{ backgroundColor: "#08bd02" }}
         onClick={() => props.setPage(props.page + 1)}
       >
         Pagina {props.page + 1}
